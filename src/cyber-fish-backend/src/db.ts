4e13bf5157a440f14a0bc0e756a0513ae423ba26
Farkas Zalan <farkasz@modularity.hu>
2025-03-16 21:29:22 +0100

    [Bug fixes]
    - small bug fix with connection string

--- src/cyber-fish-backend/src/db.ts
+++ src/cyber-fish-backend/src/db.ts
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://farkaszalan2001:SAwYhi3oQwITMC6D@cyberfish.5feja.mongodb.net/CyberFish?retryWrites=true&w=majority&appName=CyberFish");
+        await mongoose.connect(process.env.MONGODB_CONNECT_STRING || "");
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error);
